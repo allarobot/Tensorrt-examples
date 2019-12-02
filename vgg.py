4672a78fed791ac85c349983c8df76cd66ce09d4
JH <jay.cn@outlook.com>
2019-12-02 13:18:44 +0800

    vggcam model

--- vgg.py
+++ vgg.py
@@ -3,12 +3,17 @@
 """
 This sample uses a UFF vgg model to create a TensorRT Inference Engine
 
+dataset: http://pascal.inrialpes.fr/data/human/
+code example: https://github.com/jacobgil/keras-cam
+
 """
 import  tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import InputLayer,ZeroPadding2D,Convolution2D,MaxPooling2D
 from tensorflow.keras.layers.core import Flatten, Dense, Dropout, Lambda
 from config import VGG16CAMCONFIG
+
+from vgg_data import process_dataset
 
 def global_average_pooling(x):
     return tf.keras.backend.mean(x, axis = (2, 3))
@@ -73,3 +78,26 @@ class VGG16CAM():
         # Save the model
         with open(self.filename, "wb") as ofile:
             ofile.write(frozen_graph.SerializeToString())
+
+if __name__ == "__main__"
+    import cv2
+
+    net = VGG16CAM()
+
+
+    #***********
+    # data preprocess for training and testing
+    x_train, y_train, x_test, y_test = process_dataset()
+
+
+    net.create_model()
+
+
+    # Train the model on the data
+    net.model.fit(x_train, y_train, epochs=5, verbose=1)
+
+
+    # Evaluate the model on test data
+    net.model.evaluate(x_test, y_test)
+
+    net.save()
